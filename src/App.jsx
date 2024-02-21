e6da97cd27462371e68d2fd36fa2106912875d60
Shivam Patil <patilshivam156@gmail.com>
2024-02-22 01:59:24 +0530

    Add states for name, email and password and add functionality to post this data to the backend on register route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:4000"; //This will set the base url to http://localhost:4000 so that we dont need to write this for every axios call.
 
 const App = () => {
   return (
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,14 +1,55 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Registration Successful!!");
+    } catch (error) {
+      alert("Registration failed!!");
+    }
+  };
+
   return (
     <>
       <div className="mt-16">
         <h2 className="text-4xl text-center mb-4">Register</h2>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
           <button className="primary">Register</button>
         </form>
         <div className="text-center mt-5 text-gray-600">
